dbc4a35ec47b38746e3887480fdc152d76960b60
Christian <christian@chfi.se>
2020-12-15 16:14:43 -0600

    Start of tool to serialize PackedGraph for integration tests

---
+++ src/types.rs
@@ -0,0 +1,10 @@
+use gfa::{gfa::Line, parser::GFAParser};
+
+use handlegraph::{
+    handle::{Direction, Edge, Handle, NodeId},
+    handlegraph::*,
+    mutablehandlegraph::*,
+    pathhandlegraph::*,
+};
+
+use handlegraph::packedgraph::PackedGraph;
